89b29aa7a830bd46ed862bf6baed0bb4dd45cb04
listerineh <jsalvarez4@espe.edu.ec>
2022-12-04 23:11:25 -0500

    [5] Footer component

---
+++ src/components/molecules/Footer.tsx
@@ -0,0 +1,15 @@
+import SocialMedia from '../atoms/SocialMedia';
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col static w-full justify-center items-center gap-4 px-3 py-9 mb-20 bg-green">
+      <SocialMedia />
+      <p className="font-medium text-small not-italic leading-5 text-center max-w-[31.25rem]">
+        Club de Software de la Universidad de las Fuerzas Armadas ESPE 2022. All
+        rights reserverd
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
