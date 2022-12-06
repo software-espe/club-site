a4f50146f433b55cd3d8bda9ef9f0486cb5436ae
listerineh <jsalvarez4@espe.edu.ec>
2022-12-06 00:17:46 -0500

    [5] Include SocialMedia component

--- src/components/atoms/SocialMedia.tsx
+++ src/components/atoms/SocialMedia.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import LinkedinIcon from '../.././assets/icons/linkedin-icon.svg';
+import TelegramIcon from '../.././assets/icons/telegram-icon.svg';
+import WhatsAppIcon from '../.././assets/icons/whatsapp-icon.svg';
 
 const SocialMedia = () => {
-  return <div>Social Media icons</div>;
+  return (
+    <div className="flex flex-row gap-x-5">
+      <Link href="#">
+        <Image src={LinkedinIcon} alt="Linkedin icon" width="32" height="32" />
+      </Link>
+      <Link href="#">
+        <Image src={WhatsAppIcon} alt="WhatsApp icon" width="32" height="32" />
+      </Link>
+      <Link href="#">
+        <Image src={TelegramIcon} alt="Telegram icon" width="32" height="32" />
+      </Link>
+    </div>
+  );
 };
 
 export default SocialMedia;
