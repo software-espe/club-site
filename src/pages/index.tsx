aacfa19f37e477d64f985918efef701bad4f1b9b
Daniel Jaramillo <76726139+Danjar27@users.noreply.github.com>
2022-12-26 21:37:24 -0300

    [CS-12] Added new pages (#36)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,19 @@ import BaseButton from '../components/atoms/BaseButton';
 import BasePage from '../components/templates/BasePage';
 import Image from 'next/image';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const redirectToMembers = async () => {
+    await router.push('/members');
+  };
+
+  const redirectToRegister = async () => {
+    await router.push('/register');
+  };
+
   return (
     <BasePage>
       <div className="center-col md:mt-0 gap-8 mt-10">
@@ -16,11 +27,26 @@ const Home: NextPage = () => {
           </h2>
         </div>
         <div className="center lg:flex-row flex-col  gap-4 mb-32">
-          <BaseButton style={{ width: 150 }} text="Unirme" />
-          <BaseButton style={{ width: 300 }} text="Conocer a los miembros" />
+          <BaseButton
+            onClick={redirectToRegister}
+            style={{ width: 150 }}
+            text="Unirme"
+          />
+          <BaseButton
+            onClick={redirectToMembers}
+            style={{ width: 300 }}
+            text="Conocer a los miembros"
+          />
         </div>
       </div>
-      <div className="grid-background h-[300px]" />
+      <div className="w-full h-screen overflow-hidden relative">
+        <Image
+          alt="grid"
+          className="object-contain"
+          src="/images/gridBackground.svg"
+          fill={true}
+        />
+      </div>
     </BasePage>
   );
 };
---
+++ src/pages/members.tsx
@@ -0,0 +1,12 @@
+import BasePage from '../components/templates/BasePage';
+import { NextPage } from 'next';
+
+const Members: NextPage = () => {
+  return (
+    <BasePage>
+      <div className="h-screen">Members</div>
+    </BasePage>
+  );
+};
+
+export default Members;
