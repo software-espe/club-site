90e2041b8ac2dfaf96a69960786fe14f54ccbc16
Danjar27 <losteven2018@outlook.com>
2022-12-26 21:34:34 -0300

    [CS-12] Added new pages

---
+++ src/pages/register.tsx
@@ -0,0 +1,12 @@
+import BasePage from '../components/templates/BasePage';
+import { NextPage } from 'next';
+
+const Register: NextPage = () => {
+  return (
+    <BasePage>
+      <div className="h-screen">Register</div>
+    </BasePage>
+  );
+};
+
+export default Register;
